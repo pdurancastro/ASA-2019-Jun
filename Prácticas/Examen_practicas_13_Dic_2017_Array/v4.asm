0886918c27b738eeff53a7987120831e345348d0
Patricia <patryharuno@gmail.com>
2019-06-04 18:38:42 +0200

    Examen Dic 2017 Array Terminado

---
+++ Prácticas/Examen_practicas_13_Dic_2017_Array/v4.asm
@@ -0,0 +1,202 @@
+	.data
+	mensaje1: .asciiz "Introduzca el numero de elementos del array: "
+	mensaje2: .asciiz "Introduzca numero: "
+	mensaje3: .asciiz "Introduzca numero a borrar: "
+	jump: .asciiz "\n"
+	
+	.text
+	
+	la $a0,mensaje1
+	li $v0,4
+	syscall
+	
+	li $v0,5
+	syscall
+	#v0 <- numero de elementos
+	move $s0,$v0 #s0 <- Numero de elementos
+	move $a0,$s0
+	
+	jal crear_array
+	
+	move $s1,$v0 #s1<-Guardas el principio
+	
+	move $a0,$s1 #Le paso la direccion de memoria del array
+	move $a1,$s0 #Le paso el numero de elementos
+	
+	jal inicializar_array
+	
+	la $a0,mensaje3
+	li $v0,4
+	syscall
+	
+	li $v0,5
+	syscall
+	
+	move $a0,$s1
+	move $a1,$v0
+	move $a2,$zero
+	move $a3,$s0
+	jal borrar_contenido
+	move $s2,$v0
+	
+	move $a0,$s1
+	move $a1,$s0
+	jal imprimir_array
+	
+	li $v0,10
+	syscall
+	
+	
+imprimir_array:
+	#a0 <- Direccion del array
+	#a1 <- Numero de elementos	
+	
+	#Crear Pila
+	subi $sp,$sp,32
+	sw $ra,20($sp)
+	sw $fp,16($sp)
+	addiu $fp,$sp,28
+	
+	li $t0,0 #i=0
+
+bucle_lectura:
+	bge $t0,$a1,fin_bucle_lectura
+	
+	lw $t1,0($a0) #t1 <- array[i]
+	
+	#Tendre que machacar a0 para llamar a syscall por tanto guardo
+	sw $a0,0($fp)
+	move $a0,$t1
+	li $v0,1
+	syscall
+	
+	la $a0,jump
+	li $v0,4
+	syscall
+	
+	lw $a0,0($fp)
+	addiu $t0,$t0,1 #i++
+	addiu $a0,$a0,4 #siguiente posicion del array
+	b bucle_lectura
+	
+
+fin_bucle_lectura:	
+	#Borrar Pila
+	lw $ra,20($sp)
+	lw $fp,16($sp)
+	addi $sp,$sp,32
+	jr $ra
+	
+borrar_contenido:
+	#a0 <- array
+	#a1 <- valor
+	#a2 <- pos
+	#a3 <- elementos
+	
+	#Crear pila
+	subi $sp,$sp,32
+	sw $ra,20($sp)
+	sw $fp,16($sp)
+	addiu $fp,$sp,28
+	
+	blt $a2,$a3,salto1 #Lo hacemos al reves es decir si no cumple salgo sino salto
+
+	li $v0,-1
+	b fin_borrar	
+		
+salto1:
+	mul $t0,$a2,4 #<- Direccion relativa al principio
+	addu $t0,$t0,$a0 #<- Direccion cadena[pos]
+	lw $t1,0($t0) # cadena[pos]
+	bne $t1,$a1,salto2 #no se cumple el segundo if
+	#Si se cumple el if
+	sw $zero,0($t0)
+	move $v0,$a2
+	b fin_borrar
+	
+salto2:
+	#Devuelve borrar contenido el cual solo tiene cambiado pos = pos + 1
+	sw $a2,0($fp)
+	addiu $a2,$a2,1
+	jal borrar_contenido
+	lw $a2,0($fp)
+
+fin_borrar:
+	#Destruir pila
+	lw $ra,20($sp)
+	lw $fp,16($sp)
+	addiu $sp,$sp,32
+	jr $ra	
+	
+inicializar_array:
+	#a0 <- array
+	#a1 <- Numero de Elementos
+	
+	#Crear pila
+	subi $sp,$sp,32
+	sw $ra,20($sp)
+	sw $fp,16($sp)
+	addiu $fp,$sp,28
+	
+	#Necesito un 0 para poder comparar en el bucle
+	move $t0,$zero
+
+bucle_inicializar:
+	bge $t0,$a1,fin_inicializar_array
+	
+	#Guardo a0,a1 puesto que voy a llamar a syscall
+	sw $a0,0($fp)
+	#sw $a1,-4($fp)
+	
+	la $a0,mensaje2
+	li $v0,4
+	syscall
+	
+	li $v0,5
+	syscall
+	#v0 <- numero leido
+	
+	#Recupero mis elementos
+	lw $a0,0($fp)
+	#lw $a1,-4($fp)
+	
+	sw $v0,0($a0) #Guardo el primer elemento leido del array 
+	addiu $a0,$a0,4 #avanzo al siguiente en el array
+	addiu $t0,$t0,1 #i++
+	b bucle_inicializar
+
+fin_inicializar_array:
+
+	#Borrar pila
+	lw $ra,20($sp)
+	lw $fp,16($sp)
+	addiu $sp,$sp,32
+	jr $ra
+
+crear_array:
+
+	#a0 <- numero maximo de elementos
+	#v0 -> Direccion de memoria
+
+	#Creo Pila
+	subi $sp,$sp,32
+	sw $ra,20($sp)
+	sw $fp,16($sp)
+	addiu $fp,$sp,28
+	
+	sw $a0,0($fp)
+	
+	mul $a0,$a0,4
+	li $v0,9	#Reservar en el monton
+	syscall
+	
+	lw $a0,0($fp)	
+	
+	#Destruyo pila
+	lw $ra,20($sp)
+	lw $fp,16($sp)
+	addi $sp,$sp,32
+	jr $ra
+	
+
+	
