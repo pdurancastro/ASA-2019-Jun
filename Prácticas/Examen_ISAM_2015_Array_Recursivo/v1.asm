ac9b91f58f4981b27103892a0fb475753ac1e5a8
Patricia <patryharuno@gmail.com>
2019-06-06 13:03:57 +0200

    Examen_ISAM_2015_Array_recursivo Intercambiar terminado

--- Prácticas/Examen_ISAM_2015_Array_Recursivo/v1.asm
+++ Prácticas/Examen_ISAM_2015_Array_Recursivo/v1.asm
@@ -1,4 +1,3 @@
-#04-6-2015
 
 	.data
 	
@@ -14,7 +13,7 @@
 	#v0 -> devuelve la posicion del elemento 3
 	
 	move $a0,$v0	#posicion
-	addiu $a1,$a0,1 #posicion +1
+	addiu $a1,$v0,1 #posicion +1
 	la $a2, array
 	jal intercambiar
 	
@@ -24,18 +23,25 @@
 	
 	
 intercambiar:
-	#a0 ->i
-	#a1 ->j
+	#a0 -> i
+	#a1 -> j
 	#a2 ->array
 	subi $sp,$sp,32
 	sw $ra,0($sp)
 	sw $fp,4($sp)
 	addiu $fp,$sp,28
 	
+	mul $t0,$a0,4		#--> i*4
+	addu $t1,$t0,$a2	#--> direccion array[i]
+	lw $t2,0($t1)		#--> Valor del array[i]
 	
+	mul $t3,$a1,4 		#--> j*4
+	addu $t4,$t3,$a2	#--> direccion array[j]
+	lw $t5,0($t4)		#--> Valor del array[j]
 	
-	
-fin_intercambiar:	
+	sw $t2,0($t4)		#--> Valor del array[i] = direccion array[j]
+	sw $t5,0($t1)		#--> Valor del array[j] = direccion array[i]
+		
 	lw $ra,0($sp)
 	lw $fp,4($sp)
 	addiu $sp,$sp,32
@@ -70,7 +76,7 @@ salto_1:
 	addu $t0,$t0,$a0 	# <-Direccion cadena[pos] 
 	lw $t1,0($t0) 		# <-cadena[pos]
 	
-	bne $t0,$a1,salto_2
+	bne $t1,$a1,salto_2
 	
 	move $v0,$a2		#v0 -> devuelve la pos del elemento a buscar
 	b fin_buscar_desde
